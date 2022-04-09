23b9268da867e201ecab530a65b77f1f6fbf044d
PranoySarker <pranoysarker01@gmail.com>
2022-04-09 13:27:47 +0600

    added onAuthStateChanged

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import useFirebase from '../../hooks/useFirebase';
 
-const login = () => {
+
+
+const Login = () => {
+    const { signInWithGoogle } = useFirebase();
     return (
         <div>
             <h1>Please Login</h1>
+            <div style={{ margin: '20px' }}>
+                <button onClick={signInWithGoogle}>Google Sign In</button>
+            </div>
+            <form>
+                <input type="email" name="" id="" placeholder='Your Email' />
+                <br />
+                <input type="password" name="" id="" placeholder='Type your Password' />
+                <br />
+                <input type="submit" value="Login" />
+            </form>
         </div>
     );
 };
 
-export default login;
+export default Login;
